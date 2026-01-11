e59171748dde5bc50460f754ae50fd8c30d6bff9
lord-laminat <rychkin06@gmail.com>
2026-01-11 13:37:22 +0300

    backend/user_repo: add WebApi registration controller

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -1,22 +1,24 @@
-using backend.src.Middlewares;
+using Application.Users;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOpenApi();
+
+builder.Configuration
+    .AddEnvironmentVariables();
+
+builder.Services
+    .AddScoped<RegisterUserHandler>()
+    .AddScoped<IUserRepository, UserRepository>()
+    .AddDbContext<AppDbContext>(options => options.UseNpgsql(
+        builder.Configuration.GetConnectionString("Default")
+    ))
+    .AddControllers(options => {
+        options.Filters.Add<ExceptionMappingFilter>();
+    });
+
+// After building
+
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi("/openapi");
-}
-
-app.Map("/about", async (context) =>
-{
-    context.UseMiddleware<AboutMiddleware>();
-});
-
-app.Map("/", async (context) =>
-{
-    context.UseMiddleware<HelloMiddleware>();
-});
 
 app.Run();
